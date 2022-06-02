cd71fd073a9c0b5f13b338f3d353d167df425deb
Alex Ferro <51444168+alexfferro@users.noreply.github.com>
2022-06-02 13:04:02 -0300

    finalizando aula 3 com o maykão

--- rocketseat/nlw-together-origin/main.js
+++ rocketseat/nlw-together-origin/main.js
@@ -32,3 +32,33 @@ window.addEventListener('scroll', function(){
     header.classList.remove('scroll')
   }
 })
+
+/* TESTIMONIALS CARROUSEL */
+
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+/* SCROLL REVEAL */
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(`
+#home .image, #home .text,
+#about .image, #about .text,
+#services header, #services .card,
+#testimonials header, #testimonials .testimonials,
+#contact .text, #contact .links
+`,
+
+{ interval: 100})
