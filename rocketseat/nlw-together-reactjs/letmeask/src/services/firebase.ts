3a7de9a7a29831642894923f5a03ec5580aa850f
Alex Ferro <alex.ferreira.ferro@gmail.com>
2022-06-05 12:49:59 -0300

    segunda aula com o diegão

--- rocketseat/nlw-together-reactjs/letmeask/src/services/firebase.ts
+++ rocketseat/nlw-together-reactjs/letmeask/src/services/firebase.ts
@@ -4,16 +4,18 @@ import 'firebase/compat/auth';
 import 'firebase/compat/database';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+  apiKey: "AIzaSyB2LrdnXCo9hbmalrxVtkg-BKbdSi9WSa0",
+  authDomain: "letmeask-ce969.firebaseapp.com",
+  databaseURL: "https://letmeask-ce969-default-rtdb.firebaseio.com",
+  projectId: "letmeask-ce969",
+  storageBucket: "letmeask-ce969.appspot.com",
+  messagingSenderId: "153325775391",
+  appId: "1:153325775391:web:89a587b1bd8a1e0652832a"
 };
 
 firebase.initializeApp(firebaseConfig);
 
-export const auth = firebase.auth();
-export const database = firebase.database();
+const auth = firebase.auth();
+const database = firebase.database();
+
+export { firebase, auth, database }
