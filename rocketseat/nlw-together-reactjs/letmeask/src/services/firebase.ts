5c86f576703b01420f4c52654c95b62033bd9369
Alex Ferro <alex.ferreira.ferro@gmail.com>
2022-06-08 12:32:51 -0300

    Ultima aula do reactJs

--- rocketseat/nlw-together-reactjs/letmeask/src/services/firebase.ts
+++ rocketseat/nlw-together-reactjs/letmeask/src/services/firebase.ts
@@ -4,13 +4,13 @@ import 'firebase/compat/auth';
 import 'firebase/compat/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB2LrdnXCo9hbmalrxVtkg-BKbdSi9WSa0",
-  authDomain: "letmeask-ce969.firebaseapp.com",
-  databaseURL: "https://letmeask-ce969-default-rtdb.firebaseio.com",
-  projectId: "letmeask-ce969",
-  storageBucket: "letmeask-ce969.appspot.com",
-  messagingSenderId: "153325775391",
-  appId: "1:153325775391:web:89a587b1bd8a1e0652832a"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
