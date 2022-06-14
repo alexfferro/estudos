c580503e70e19e411d9e7a383f64bf104b566d80
Alex Ferro <alex.ferreira.ferro@gmail.com>
2022-06-13 22:24:25 -0300

    aula 5 nlw return origin

--- rocketseat/nlw-return-origin/main.js
+++ rocketseat/nlw-return-origin/main.js
@@ -4,6 +4,32 @@ onScroll()
 function onScroll(){
   showNavOnScroll()
   showBackToTopOnScroll()
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section){
+  const targetLine = scrollY + (innerHeight / 2);
+
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+  const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+  const menuElement = document.querySelector(`.menu a[href*=${section.getAttribute('id')}]`)
+
+  menuElement.classList.remove('active')
+  if(sectionBoundaries){
+    menuElement.classList.add('active')
+  }
 }
 
 function showNavOnScroll(){
@@ -29,7 +55,6 @@ function openMenu(){
 function closeMenu(){
   document.body.classList.remove('menu-expanded')
 }
-
 
 ScrollReveal({
   origin: 'top',
