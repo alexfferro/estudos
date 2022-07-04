8ecb32658e0c4a43dbffab5246d72111e59960ef
Alex Ferro <alex.ferreira.ferro@gmail.com>
2022-07-04 16:15:43 -0300

    bestMeApp finalizado

--- rocketseat/discover/projetos/bestMeApp/index.js
+++ rocketseat/discover/projetos/bestMeApp/index.js
@@ -6,7 +6,37 @@ const questions = [
 ]
 
 const ask = ( index = 0) => {
-  process.stdout.write(questions[index] + "\n")
+  process.stdout.write("\n" + questions[index] + ' >> ')
 }
 
 ask()
+
+const answers = []
+process.stdin.on("data", data => {
+  answers.push(data.toString().trim())
+  if(answers.length < questions.length) {
+    ask(answers.length)
+  }else{
+    console.log(answers)
+    process.exit()
+  }
+})
+
+process.on('exit', () => {
+  console.log(`
+  Bacana Alex!
+
+  O que você aprendeu hoje foi:
+  ${answers[0]}
+
+  O que te aborreceu e você poderia melhorar foi:
+  ${answers[1]}
+
+  O que te deixou mais feliz hoje:
+  ${answers[2]}
+
+  Você ajudou ${answers[3]} pessoas hoje!!
+
+  Volte amanhã para novas reflexões
+  `)
+})
