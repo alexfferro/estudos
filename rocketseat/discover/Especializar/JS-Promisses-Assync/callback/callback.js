7f42248130ea9e0cf3917804d6768c32aadc397c
Alex Ferreira <alex.ferreira.ferro@gmail.com>
2022-09-08 12:47:08 -0300

    Aula sobre callback functions, atualizada

---
+++ rocketseat/discover/Especializar/JS-Promisses-Assync/callback/callback.js
@@ -0,0 +1,20 @@
+// Funções aceitam qualquer tipo de dado como argumento
+function imprimirDado(dado) {
+  console.log(dado())
+}
+
+// imprimirDado(1)
+// imprimirDado('texto')
+// imprimirDado(true)
+// imprimirDado({nome: 'alex'})
+// imprimirDado([1, 2, 3])
+
+// é possível também executar uma função dentro de outra função.
+
+imprimirDado(function () {
+  return 'Olá Mundo'
+})
+
+/** 
+ * Então basicamente, uma callback function é uma função que chama outra função.
+*/
