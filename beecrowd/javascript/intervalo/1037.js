36c4dc1b15378a620141cd5e4bb19b1c3e576052
Alex Silva <alex.ferreira.ferro@gmail.com>
2023-01-09 17:09:50 -0300

    beecrowd - exercicio 1037 feito

---
+++ beecrowd/javascript/intervalo/1037.js
@@ -0,0 +1,22 @@
+var input = require('fs').readFileSync('stdin', 'utf8')
+var lines = input.split(' ');
+
+[a] = lines.map(item => {
+  return Number(item);
+})
+
+if (a >= 0 & a <= 25){
+  console.log('Intervalo [0,25]')
+}
+else if (a > 25 & a <=50){
+  console.log('Intervalo (25,50]')
+}
+else if (a > 50 & a <=75){
+  console.log('Intervalo (50,75]')
+}
+else if (a > 75 & a <= 100){
+  console.log('Intervalo (75,100]')
+}
+else{
+  console.log('Fora de intervalo')  
+}
